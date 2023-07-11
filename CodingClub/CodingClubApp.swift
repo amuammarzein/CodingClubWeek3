973629e0e87f4da5836a74f779f738a6d887a95a
Aang Muammar Zein <amuammarzein@gmail.com>
2023-07-11 20:46:15 +0700

    Initial Commit

---
+++ CodingClub/CodingClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingClubApp.swift
+//  CodingClub
+//
+//  Created by Aang Muammar Zein on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
